93e3af9d154538b41bd2004b8e3362c41649611e
Porchheng <porchheng914@gmail.com>
2025-03-19 15:40:37 +0700

    START CODE + RidePreference

---
+++ lib/model/ride/ride_pref.dart
@@ -0,0 +1,43 @@
+import '../location/locations.dart';
+
+///
+/// This model describes a ride preference.
+/// A ride preference consists of the selection of a departure + arrival + a date and a number of passenger
+///
+class RidePreference {
+  final Location departure;
+  final DateTime departureDate;
+  final Location arrival;
+  final int requestedSeats;
+
+  const RidePreference(
+      {required this.departure,
+      required this.departureDate,
+      required this.arrival,
+      required this.requestedSeats});
+
+  RidePreference.copy(RidePreference other)
+      : departure = other.departure,
+        departureDate = other.departureDate,
+        arrival = other.arrival,
+        requestedSeats = other.requestedSeats;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is RidePreference &&
+        other.departure == departure &&
+        other.arrival == arrival;
+  }
+
+  @override
+  int get hashCode => departure.hashCode ^ arrival.hashCode;
+
+  @override
+  String toString() {
+    return 'RidePref(departure: ${departure.name}, '
+        'departureDate: ${departureDate.toIso8601String()}, '
+        'arrival: ${arrival.name}, '
+        'requestedSeats: $requestedSeats)';
+  }
+}
