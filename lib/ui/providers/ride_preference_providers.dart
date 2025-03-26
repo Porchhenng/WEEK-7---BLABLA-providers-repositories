61ee97ae4527e731060392d0caf8fd9b07e6f39c
Porchheng <porchheng914@gmail.com>
2025-03-26 13:34:18 +0700

    BLA - 203

--- lib/ui/providers/ride_preference_providers.dart
+++ lib/ui/providers/ride_preference_providers.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:week_3_blabla_project/model/ride/ride_pref.dart';
-import 'package:week_3_blabla_project/repository/mock/mock_locations_repository.dart';
-import 'package:week_3_blabla_project/repository/ride_preferences_repository.dart';
+import 'package:week_3_blabla_project/data/repository/mock/mock_locations_repository.dart';
+import 'package:week_3_blabla_project/data/repository/ride_preferences_repository.dart';
 import 'package:week_3_blabla_project/ui/providers/async_value.dart';
 
 class RidesPreferencesProvider extends ChangeNotifier {
